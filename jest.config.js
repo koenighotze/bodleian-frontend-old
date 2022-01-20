2da59b2b1631ee496cf7f87c5a243a7e72941df7
David Schmitz <davidschmitz@posteo.de>
2022-01-20 16:29:20 +0100

    Initial setup

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'json'
+  ],
+  testMatch: [
+    '**/src/**/*.test.js'
+  ],
+  testEnvironment: 'node',
+  resetMocks: true,
+  resetModules: true,
+  verbose: false,
+  collectCoverage: true,
+  coverageReporters: [
+    'lcov',
+    'text',
+    'text-summary'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js'
+  ],
+  roots: [
+    'src/'
+  ]
+}
