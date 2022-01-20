0abe3aae6a950c0128aa209f9bce1a9539073b84
David Schmitz <davidschmitz@posteo.de>
2022-01-20 16:49:10 +0100

    Add workflows

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React from 'react';
+import React from 'react'
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        Welcome to Bodleian
-      </header>
-      <div className="container">
-        Work in progress
-      </div>
+      <header className="App-header">Welcome to Bodleian</header>
+      <div className="container">Work in progress</div>
     </div>
   )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,3 @@
 test('renders learn react link', () => {
   // TODO
-});
+})
